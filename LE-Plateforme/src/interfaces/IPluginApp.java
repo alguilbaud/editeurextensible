dbe973944500e3506c056bfbcb1d34f8c238ca7d
guilbaud.alexis@gmail.com <guilbaud.alexis@gmail.com@a6af5479-7acb-53d8-d243-781e6d1f6e8c>
2014-04-28 15:16:13 +0000

    ajout javadoc LE-Plateforme

--- LE-Plateforme/src/interfaces/IPluginApp.java
+++ LE-Plateforme/src/interfaces/IPluginApp.java
@@ -2,7 +2,15 @@ package interfaces;
 
 import chargement.Loader;
 
+/**
+ * L'interface IPluginApp qui étend IPlugin, doit être implémentée par les plugins de type "Application".
+ * @author Benjamin Grouhan, Alexis Guilbaud, Kevin Mokili
+ */
 public interface IPluginApp extends IPlugin {
 	
+	/**
+	 * Méthode permettant de démarrer l'application.
+	 * @param l Instance du Loader utiliséé pour le lancement.
+	 */
 	public void demarrer(Loader l);
 }
