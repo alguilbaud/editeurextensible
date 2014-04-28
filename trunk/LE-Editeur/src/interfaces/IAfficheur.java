14772f72de1164b70e091e917b61a244e4b7c065
guilbaud.alexis@gmail.com <guilbaud.alexis@gmail.com@a6af5479-7acb-53d8-d243-781e6d1f6e8c>
2014-04-28 14:29:24 +0000

    ajout javadoc LE-Editeur

--- trunk/LE-Editeur/src/interfaces/IAfficheur.java
+++ trunk/LE-Editeur/src/interfaces/IAfficheur.java
@@ -3,10 +3,30 @@ package interfaces;
 import javax.swing.JComponent;
 import javax.swing.JTextArea;
 
-
+/**
+ * L'interface IAfficheur permet de représenter les plugins de type "Afficheur", c'est à dire ceux chargés d'ajouter de l'information 
+ * sur l'application dans un composant graphique.
+ * @author Benjamin Grouhan, Alexis Guilbaud, Kevin Mokili
+ */
 public interface IAfficheur extends IPluginTerminal{
+	/**
+	 * Méthode qui devra créer le JComponent contenant les informations à afficher.
+	 * @param textArea La zone de texte sur laquelle il est possible de récupérer des informations
+	 * @return Le JComponent créé
+	 */
 	public JComponent creerJComponent(JTextArea textArea);
 	
+	/**
+	 * Méthode qui devra retourner, sous forme de String, la position sur laquelle le composant graphique doit être placé sur l'application.
+	 * @return Une String avec pour valeurs possibles : 
+	 * <ul>
+	 * <li> North </li>
+	 * <li> East </li>
+	 * <li> South </li>
+	 * <li> West </li>
+	 * </ul>
+	 * @return
+	 */
 	public String getPosition();
 	
 }
--- trunk/LE-Editeur/src/interfaces/IModificateur.java
+++ trunk/LE-Editeur/src/interfaces/IModificateur.java
@@ -1,7 +1,16 @@
 package interfaces;
 
-
+/**
+ * L'interface IModificateur permet de représenter les plugins de type "Modificateur", c'est à dire permettant de modifier
+ * le contenu du texte déjà écrit dans l’éditeur.
+ * @author Benjamin Grouhan, Alexis Guilbaud, Kevin Mokili
+ */
 public interface IModificateur extends IPluginTerminal{
+	/**
+	 * Permet de modifier le texte entré en paramètres.
+	 * @param texte Le texte à modifier
+	 * @return Le texte une fois modifié
+	 */
 	public String modifier(String texte);
 
 }
