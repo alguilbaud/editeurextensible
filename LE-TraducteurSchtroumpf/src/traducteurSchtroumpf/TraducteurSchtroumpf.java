72cbac2a28731ac4e10d79d9f75382e22fc57008
guilbaud.alexis@gmail.com <guilbaud.alexis@gmail.com@a6af5479-7acb-53d8-d243-781e6d1f6e8c>
2014-04-28 16:10:51 +0000

    ajout javadoc Le-Traducteur

--- LE-TraducteurSchtroumpf/src/traducteurSchtroumpf/TraducteurSchtroumpf.java
+++ LE-TraducteurSchtroumpf/src/traducteurSchtroumpf/TraducteurSchtroumpf.java
@@ -5,8 +5,19 @@ import java.util.Random;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
+/**
+ * La classe TraducteurSchtroumpf est un exemple de plugin de type "Modificateur", permettant de traduire un texte 
+ * en "Schtroumpf", c'est à dire de remplacer certains mots par "schtroumpf" de manière aléatoire.
+ * @author Benjamin Grouhan, Alexis Guilbaud, Kevin Mokili
+ * @see IModificateur
+ */
 public class TraducteurSchtroumpf implements IModificateur{
 	
+	/**
+	 * Implémentation de la méthode modifier(String) de l'interface IModificateur, remplaçant certains mots du
+	 * texte passé en paramètres par le mot "schtroumpf" de manière aléatoire.
+	 * @see IModificateur#modifier(String)
+	 */
 	public String modifier(String texte){
 		Pattern p = Pattern.compile("\\w+");
 	    Matcher m = p.matcher(texte);
